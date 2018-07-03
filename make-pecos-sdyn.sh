5e4597c4569b083ced71397928f6fc4c29af0c36
Jose F. Morales <jfmcjf@gmail.com>
2018-07-03 13:51:43 +0200

    Alternative script to compile and collect .so/.dylib in the binary directory

---
+++ make-pecos-sdyn.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+PECOS="/Users/jpg/Research/LP/clptools/predabs/pecos"
+#CIAOROOT="/Users/jpg/local/ciao-devel"
+#CIAOPATH="/Users/jpg/ciao"
+
+# copy programs from pecos 
+
+cp $PECOS/pe/*.pl pe
+
+# copy preprocessing programs
+cp $PECOS/smt2chc/* smt2chc
+
+# Binaries
+
+cd bin
+
+# Copy scripts
+cp $PECOS/bin/starexec_run_lia.sh .
+cp $PECOS/bin/starexec_run_lra.sh .
+# Re-compile everything and put binaries and dynamic libraries in bin/
+ciaoc_sdyn $CIAOPATH/chclibs/src/thresholds1.pl
+ciaoc_sdyn $CIAOPATH/chclibs/src/cpascc.pl
+ciaoc_sdyn $CIAOPATH/chclibs/src/qa.pl
+ciaoc_sdyn $CIAOPATH/RAHFT/src/insertprops.pl
+ciaoc_sdyn $CIAOPATH/RAHFT/src/raf
+#
+ciaoc_sdyn $PECOS/smt2chc/chcNorm.pl
+ciaoc_sdyn $PECOS/pe/peunf_smt_2.pl
+ciaoc_sdyn $PECOS/pe/counterExample.pl
+ciaoc_sdyn $PECOS/pe/props.pl
+ciaoc_sdyn $PECOS/pe/tpclp.pl
+
